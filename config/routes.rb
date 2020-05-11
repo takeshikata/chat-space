67ecd67a6d890cb37329c36b1c7e1c2e06d61cec
takeshikata <kataoka1053559@gmail.com>
2020-05-11 18:24:07 +0900

    create-group-function

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root "messages#index"
+  root 'groups#index'
+  #root "messages#index" ChatSpaceの場合はメッセージ一覧ではなく、グループ一覧を表示させる
   resources :users, only: [:edit, :update]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
