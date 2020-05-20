45640875b9630d94cc60c74a1cae04248093c12d
takeshikata <kataoka1053559@gmail.com>
2020-05-20 20:01:47 +0900

    upload-to-S3

--- Gemfile
+++ Gemfile
@@ -80,3 +80,5 @@ gem "jquery-rails"
 
 gem 'carrierwave'
 gem 'mini_magick'
+
+gem 'fog-aws'
--- config/deploy.rb
+++ config/deploy.rb
@@ -17,7 +17,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                keys: ['~/.ssh/ChatSpace.pem'] 
+                keys: ['~/.ssh/ChatSpace.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
@@ -26,10 +26,25 @@ set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
+# secrets.yml用のシンボリックリンクを追加
+set :linked_files, %w{ config/secrets.yml }
+
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
-task :restart do
-  invoke 'unicorn:restart'
-end
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+  # ローカル環境にあるconfig/secrets.ymlを本番環境のshared/config/secrets.ymlに反映するための設定
+  desc 'upload secrets.yml'
+  task :upload do
+    on roles(:app) do |host|
+      if test "[ ! -d #{shared_path}/config ]"
+        execute "mkdir -p #{shared_path}/config"
+      end
+      upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
+    end
+  end
+  before :starting, 'deploy:upload'
+  after :finishing, 'deploy:cleanup'
 end
